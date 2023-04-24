5f12091b88d06645ef24df52b14f55d1b19d5399
Setthawut <setthawut.kitd@mail.kmutt.ac.th>
2023-04-24 11:24:20 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main (string[] args){
+        Console.Write("Enter the number of items in the bag: ");
+        int numItems = int numItems = Convert.Parse(Console.ReadLine());
+        string[] items = new string[numItems];
+        for(int i = 0; i < numItems; i++)
+        Console.WriteLine($"Item {i + 1}:");
+        Console.Write("item name: ");
+        string itemName = Console.ReadLine();
+        Console.Write("Item type (except 'ShowAll'):");
+        string itemType = Console.ReadLine();
+        if (!items.ContainsKey(itemType))
+    }
+}
